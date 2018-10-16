c313e0a2d8cc96bc5c80926581dbeaebb6932cf9
victorcezario97 <victorscezario@hotmail.com>
2018-10-16 16:08:33 -0300

    Made some design changes and passed the information from the activity_main to the activity_quem_pagou.

--- app/src/main/java/com/example/cruwal/repapplication/MainActivity.java
+++ app/src/main/java/com/example/cruwal/repapplication/MainActivity.java
@@ -5,9 +5,7 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,15 +15,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //get the spinner from the xml.
-        //Spinner dropdown = findViewById(R.id.quem_pagou_spinner);
-//create a list of items for the spinner.
-        String[] items = new String[]{"Portuga", "Lari", "Sempe", "Jesus", "Watts", "Jun", "Floyd", "Bordel", "Xorin", "Parto", "Murilo", "Daniel", "Cilene", "Perdido", "Felina"};
-//create an adapter to describe how the items are displayed, adapters are used in several places in android.
-//There are multiple variations of this, but this is the basic variant.
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-//set the spinners adapter to the previously created one.
-        //dropdown.setAdapter(adapter);
     }
 
     public void showDatePickerDialog(View v) {
@@ -40,7 +29,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void quemPagouActivity(View view) {
-        Intent intent = new Intent(this, quemPagouActivity.class);
+        Intent intent = new Intent(this, quaisPagaraoActivity.class);
+        EditText quemPagou, quanto, oQue;
+        TextView quando;
+
+        quemPagou = findViewById(R.id.quem_pagou_ET);
+        quanto = findViewById(R.id.quanto_ET);
+        oQue = findViewById(R.id.o_que_ET);
+        quando = findViewById(R.id.quando_data_TV);
+
+        intent.putExtra("quem", quemPagou.getText().toString());
+        intent.putExtra("quanto", quanto.getText().toString());
+        intent.putExtra("oque", oQue.getText().toString());
+        intent.putExtra("quando", quando.getText().toString());
 
         startActivity(intent);
     }
--- app/src/main/java/com/example/cruwal/repapplication/quaisPagaraoActivity.java
+++ app/src/main/java/com/example/cruwal/repapplication/quaisPagaraoActivity.java
@@ -1,5 +1,6 @@
 package com.example.cruwal.repapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,15 +11,41 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 //Activity com checkBoxes pra escolher para quais moradores deve ser distribuido o valor
-public class quemPagouActivity extends AppCompatActivity {
+public class quaisPagaraoActivity extends AppCompatActivity {
 
     //Array com os nomes dos moradores selecionados
     ArrayList<String> nomes = new ArrayList<>();
+    //TextView onde a lista será exibida
+    TextView tv;
+
+    StringBuilder sb = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quem_pagou);
+
+        String quem, oque, quanto, quando;
+        TextView quemTV, oqueTV, quantoTV, quandoTV;
+
+        tv = findViewById(R.id.testTV);
+        Intent intent = getIntent();
+
+        quem = intent.getStringExtra("quem");
+        quanto = intent.getStringExtra("quanto");
+        oque = intent.getStringExtra("oque");
+        quando = intent.getStringExtra("quando");
+
+        quemTV = findViewById(R.id.quem_pagou_TV);
+        oqueTV = findViewById(R.id.o_que_TV);
+        quantoTV = findViewById(R.id.quanto_TV);
+        quandoTV = findViewById(R.id.quando_data_TV_aqp);
+
+        quemTV.setText(quem);
+        oqueTV.setText(oque);
+        quandoTV.setText(quando);
+        quantoTV.setText(quanto);
+
     }
 
     public void onCheckBoxClicked(View view) {
@@ -36,10 +63,10 @@ public class quemPagouActivity extends AppCompatActivity {
 
     //Funcao que mostra como a lista de nomes está em dado momento
     public void showArray(View view) {
-        //TextView onde a lista será exibida
-        TextView tv = findViewById(R.id.testTV);
 
-        StringBuilder sb = new StringBuilder();
+
+
+
 
         for(String s : nomes){
             sb.append(s);
